b544a39b6bf7b10ee4e73f61b4c02228462d901f
d3vv3 <jaimecondel@gmail.com>
2022-07-02 23:01:01 +0200

    feat: navigation and play page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import {Logo} from "../components";
-import Link from "next/link"
+import Head from "next/head";
+import { Logo } from "../components";
+import Link from "next/link";
 export default function Home() {
   return (
-
     <div>
-
       <Head>
         <title>DroonkGame</title>
         <meta name="description" content="A mi me están grabando" />
@@ -13,27 +11,23 @@ export default function Home() {
       </Head>
 
       <main>
-        <div className='welcome-screen'>
+        <div className="welcome-screen">
           <div className="logo-container">
-            <div className='welcome-logo'>
-            <Logo />
-            <h1>the game</h1>
+            <div className="welcome-logo">
+              <Logo />
+              <h1>the game</h1>
             </div>
           </div>
           <div className="buttons">
-          <Link href="/rules">
-          <button
-              className="input-buttons">NORMAS</button>
-          </Link>
-          <Link href="/play">
-            <button
-              className="input-buttons">EMPIEZA A JUGAR</button>
-          </Link>
-</div>        
-</div>
+            <Link href="/rules">
+              <button className="input-buttons">NORMAS</button>
+            </Link>
+            <Link href="/play">
+              <button className="input-buttons">EMPIEZA A JUGAR</button>
+            </Link>
+          </div>
+        </div>
       </main>
     </div>
-  )
+  );
 }
-
-
