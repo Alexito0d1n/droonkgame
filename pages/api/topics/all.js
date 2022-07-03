2435f1bb0fa0fcc1542decfc6d99d0932d5cbb4f
d3vv3 <jaimecondel@gmail.com>
2022-07-03 18:43:29 +0200

    feat: added buffer to avoid most recent answered questions, refactor memory

--- pages/api/topics/all.js
+++ pages/api/topics/all.js
@@ -2,17 +2,15 @@ import { supabase } from "/utils";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
-    const { id } = req.query;
+    console.log(req.body);
+    // const { level, category, language } = req.body
 
-    const { data, error } = await supabase
-      .from("questions_view")
-      .select()
-      .eq("id", id);
+    const { data, error } = await supabase.from("topic").select();
 
     // console.log(data);
     // console.log(error);
 
-    res.status(200).json(data[0]);
+    res.status(200).json(data);
   } else {
     // Handle any other HTTP method: not allowed
     res.status(405).end();
